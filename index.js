49784ed7885c65dd8fff8a750b62c3a0e5122815
riteshahlawat <riteshahlawat1@gmail.com>
2019-08-29 01:46:08 -0400

    Update

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(
+    express.static(path.join(__dirname, "public"), {
+      extensions: ["html"]
+    })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
