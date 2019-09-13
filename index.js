427e12a591d07990018d526ed2d1ec7c77bcb694
riteshahlawat <riteshahlawat1@gmail.com>
2019-09-13 10:49:13 -0400

    Optimization

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+const axios = require("axios");
+const parseString = require("xml2js").parseString;
+const fs = require("fs");
 
 // Body Parser Middleware
 app.use(express.json());
@@ -13,9 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(
-    express.static(path.join(__dirname, "public"), {
-      extensions: ["html"]
-    })
+  express.static(path.join(__dirname, "public"), {
+    extensions: ["html"]
+  })
 );
 // APIS
 app.use("/api", require("./routes/apis/apis"));
@@ -24,4 +27,47 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+  // Load route configs
+  loadRouteConfig();
 });
+
+loadRouteConfig = () => {
+  axios
+    .get(
+      "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc"
+    )
+    .then(response => {
+      parseString(response.data, (err, results) => {
+        var newJSON = results.body.route;
+        // Bus route list
+        // var newJSON = json.slice(0, json.length);
+        // Get bus route configurations
+        for (let i = 0; i < newJSON.length; i++) {
+          // For each route
+          var element = newJSON[i].$.tag;
+          var finalBusRouteConfiguration = [];
+          axios
+            .get(
+              "http://localhost:5000/api/getRouteConfiguration/" +
+                element +
+                "?stopsOnly=false"
+            )
+            .then(configResponse => {
+              let busRouteConfiguration = {};
+              busRouteConfiguration[element] = configResponse.data;
+              finalBusRouteConfiguration = [
+                ...finalBusRouteConfiguration,
+                busRouteConfiguration
+              ];
+              if (i == newJSON.length - 1) {
+                // Route list containing all routes with configs however
+                console.log("Done");
+                fs.writeFile("routeconfig.json", JSON.stringify(finalBusRouteConfiguration), err => {
+                    if (err) throw err;
+                });
+              }
+            });
+        }
+      });
+    });
+};
--- routes/apis/apis.js
+++ routes/apis/apis.js
@@ -2,38 +2,16 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const parseString = require("xml2js").parseString;
+const fs = require("fs");
 
+// TODO: CREATE MIDDLEWARE TO STORE ROUTE CONFIG LOCALLY
 router.get("/getAllRoutes/:endIndex", (req, res) => {
-  axios
-    .get(
-      "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc"
-    )
-    .then(response => {
-      parseString(response.data, (err, results) => {
-        var json = results.body.route;
-        var endIndex = req.params.endIndex;
-        // Bus route list
-        var newJSON = json.slice(0, endIndex);
-        // Get bus route configurations
-        for (let i = 0; i < newJSON.length; i++) {
-          // For each route
-          var element = newJSON[i].$.tag;
-          var finalBusRouteConfiguration = [];
-          axios
-            .get("http://localhost:5000/api/getRouteConfiguration/" + element + "?stopsOnly=false")
-            .then(configResponse => {
-                let busRouteConfiguration = {};
-                busRouteConfiguration[element] = configResponse.data;
-                finalBusRouteConfiguration = [...finalBusRouteConfiguration, busRouteConfiguration];
-                if(i==newJSON.length-1){
-                  // Route list containing all routes with configs however
-                  res.send(finalBusRouteConfiguration);
+  fs.readFile("routeconfig.json", (err, data) => {
+    if (err) throw err;
+    let finalBusRouteConfig = JSON.parse(data);
 
-                }
-            });
-        }
-      });
-    });
+    res.send(finalBusRouteConfig.slice(0, req.params.endIndex));
+  });
 });
 
 router.get("/getRouteConfiguration/:routeTag", (req, res) => {
